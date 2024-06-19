d6e312cf44a4faa84c52fabc6bcb3788252560e9
ritu-477 <ritujoharbishnoi@gmail.com>
2024-06-19 09:20:44 +0530

    change postion slider arrow

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
-      fontFamily:{
-               'rubik': "'rubik',sans serif"
-      }
+      fontFamily: {
+        'rubik': "'rubik',sans serif"
+      },
+      screens: {
+        '2xl': '1536px',
+        // Add more custom breakpoints if necessary
+      },
     },
   },
   plugins: [],
